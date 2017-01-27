65f9a89a5d5f760b4063c3bf70faacb8a2af1c04
Md. Shahadul Alam Patwary <shahadul_alam_17@outlook.com>
2017-01-27 23:12:13 +0600

    Updated files via GitHub Desktop

--- Cryptonite/Properties/AssemblyInfo.cs
+++ Cryptonite/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cryptonite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple and lightweight file protection tool.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Md. Shahadul Alam Patwary")]
 [assembly: AssemblyProduct("Cryptonite")]
